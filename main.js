e0f3e617c9dd55cfba8db564d557b3e60c35ba13
Jonathan Chen <jonoson@gmail.com>
2019-01-27 01:38:18 -0500

    update task test based on steps.

--- main.js
+++ main.js
@@ -16,15 +16,64 @@ const argv = require('minimist')(process.argv.slice(2));
 logging.installConsoleHandler();
 logging.getLogger('webdriver.http').setLevel(logging.Level.SEVERE);
 
-let ampTestConfig = {
+/*
+actions:
+  URL
+  InputThenEnter
+
+*/
+let taskConfig = {
   browser: 'chrome',
   searchKeyword: 'amp-list',
   expectedAmpViewerTitle: 'ampbyexample.com',
   targetSelector: 'a.amp_r',
+  screenshot: true,
+  width: 500,
+  height: 1000,
+  sleepAfterEachStep: 1000,
+  steps: [{
+    action: 'URL',
+    log: 'Search',
+    url: 'https://www.google.com/ncr',
+  }, {
+    action: 'InputThenEnter',
+    log: 'Input search keyword',
+    cssSelector: 'input[name="q"]',
+    inputText: 'amp-list',
+    sleepAfter: 0,
+  }, {
+    action: 'VerifyTitle',
+    log: 'Verify page title',
+    textEqualsTo: 'amp-list - Google Search',
+  }, {
+    action: 'Click',
+    log: 'Click first AMP result',
+    cssSelector: 'a.amp_r',
+  }, {
+    action: 'VerifyText',
+    log: 'Verify AMP viewer title',
+    cssSelector: '.amp-ttltxt',
+    textEqualsTo: 'ampbyexample.com',
+  }, {
+    action: 'SwitchIframe',
+    iframeId: 0, // iframe number or 'id'
+  }, {
+    action: 'Click',
+    log: 'Click Open In Playground button',
+    cssSelector: 'a.ampstart-btn',
+  }, {
+    action: 'VerifyText',
+    log: 'Verify document title',
+    cssSelector: '#document-title',
+    textEqualsTo: 'amp-list',
+  }, {
+    action: 'Screenshot',
+    log: 'Screenshot.',
+    outputFolder: 'output',
+    suffix: 'End',
+  }],
 };
 
-const width = 500;
-const height = 1000;
 let loggerStep = 1;
 
 function log(message) {
@@ -32,17 +81,29 @@ function log(message) {
   loggerStep++;
 }
 
-async function createDriver(browser) {
+function printUsage() {
+  let usage = `
+Usage: node main.js
+
+Options:
+  --runs=NUM_OF_RUNS\tNumber of test runs.
+  --output=FILE\t\tPath to the output file.
+  --config=FILE\t\tPath to the task config in JSON format.
+  `;
+  console.log(usage);
+}
+
+async function createDriver(config) {
   let driver;
   try {
-    switch(browser) {
+    switch (config.browser) {
       case 'firefox':
         let options = new firefox.Options()
-            // .setPreference('devtools.responsive.html.displayedDeviceList',
-            //   '{"added":["Nexus 5X"],"removed":[]}')
-            .setPreference('devtools.responsive.userAgent',
-              'Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1')
-            .setBinary(firefox.Channel.RELEASE);
+          // .setPreference('devtools.responsive.html.displayedDeviceList',
+          //   '{"added":["Nexus 5X"],"removed":[]}')
+          .setPreference('devtools.responsive.userAgent',
+            'Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1')
+          .setBinary(firefox.Channel.RELEASE);
         driver = new Builder()
           .forBrowser('firefox')
           .setFirefoxOptions(options)
@@ -52,107 +113,114 @@ async function createDriver(browser) {
       case 'chrome':
       default:
         driver = new Builder()
-        .forBrowser('chrome').setChromeOptions(
-          new chrome.Options()
-          .setMobileEmulation({
-            deviceName: 'Pixel 2'
-          }).windowSize({
-            width,
-            height
-          }))
-        .build();
+          .forBrowser('chrome').setChromeOptions(
+            new chrome.Options()
+            .setMobileEmulation({
+              deviceName: 'Pixel 2'
+            }).windowSize({
+              width: config.width,
+              height: config.height
+            }))
+          .build();
         break;
     }
-  } catch(err) {
+
+    await driver.manage().window().setRect({
+      width: config.width,
+      height: config.height,
+      x: 100,
+      y: 0
+    });
+
+  } catch (err) {
     console.error(err);
   } finally {
     return driver;
   }
 }
 
-async function runGoogleSearchTest(config, context) {
+async function runTask(config, context) {
   let driver;
   try {
-    driver = await createDriver(config.browser);
+    assert(config.steps);
 
-    await driver.manage().window().setRect({
-      width: width,
-      height: height,
-      x: 100,
-      y: 0
-    });
-
+    driver = await createDriver(config);
     const actions = driver.actions({
       bridge: true
     });
-
-    log('Search...');
     await driver.sleep(2000);
 
-    await driver.get('https://www.google.com/ncr');
-    await driver.findElement(By.name('q')).sendKeys(config.searchKeyword, Key.RETURN);
-
-    await driver.wait(until.titleIs(config.searchKeyword + ' - Google Search'), 3000);
-
-    log('Received search results.');
-
-    let body = await driver.wait(until.elementLocated(By.css('body')), 3000);
-    let mainDom = await driver.wait(until.elementLocated(By.css('#tophf')), 3000);
-
-    log('mainDom result found.');
-
-    let targetDom = await driver.findElement(By.css(config.targetSelector));
-    log('first amp result found.');
-
-    assert.equal(await targetDom.getTagName(), 'a');
-    assert.equal(await targetDom.getAttribute('class'), 'C8nzq BmP5tf amp_r');
-
-    await targetDom.click();
-    log('Clicked first AMP result.');
-
-    await driver.sleep(2000);
-
-    let ampViewerTitle = await driver.wait(until.elementLocated(By.css('.amp-ttltxt')), 3000);
-    assert.equal(await ampViewerTitle.getText(), config.expectedAmpViewerTitle);
-    log('Saw AMP viewer title: ' + await ampViewerTitle.getText());
-
-    // Switch to first iframe.
-    // driver.switchTo().frame(0);
-    //
-    // let ampContent = await driver.wait(until.elementLocated(By.css('img[alt="AMP by Example"]')), 3000);
-    // log('Saw AMP by example content.');
-    await driver.sleep(3000);
-
-    log('Task Complete.');
-
+    for (var i = 0; i < config.steps.length; i++) {
+      let step = config.steps[i];
+      let targetDom;
+      log(step.log || 'No log.');
+      switch (step.action) {
+        case 'URL':
+          await driver.get(step.url);
+          break;
+        case 'InputThenEnter':
+          await driver
+            .findElement(By.css(step.cssSelector))
+            .sendKeys(step.inputText, Key.RETURN);
+          break;
+        case 'Click':
+          targetDom = await driver.findElement(By.css(step.cssSelector));
+          await targetDom.click();
+          break;
+        case 'VerifyTitle':
+          await driver.wait(until.titleIs(step.textEqualsTo), 3000);
+          break;
+        case 'VerifyText':
+          targetDom = await driver.wait(until.elementLocated(By.css(step.cssSelector)), 3000);
+          assert.equal(await targetDom.getText(), step.textEqualsTo);
+          break;
+        case 'SwitchIframe':
+          await driver.switchTo().frame(step.iframeId);
+          break;
+        case 'Screenshot':
+          let image = await driver.takeScreenshot();
+          require('fs').writeFileSync(
+            `${step.outputFolder}/screenshot-RUN_${context.run}-${step.suffix}.png`,
+            image, 'base64');
+          break;
+        default:
+          throw new Error(`action ${step.action} is not supported.`);
+          break;
+      }
+      if (step.sleepAfter) {
+        await driver.sleep(step.sleepAfter);
+      }
+      if (config.sleepAfterEachStep) {
+        await driver.sleep(config.sleepAfterEachStep);
+      }
+    }
   } catch (err) {
     console.error(err);
 
   } finally {
-    if (driver && context.screenshot) {
-      let image = await driver.takeScreenshot();
-      require('fs').writeFileSync(`output/run-${context.run}.png`, image, 'base64');
-      log('Screenshot took.');
-    }
     await driver && driver.quit();
-    log('Driver quit.');
+    log('Complete.');
   }
 }
 
-// Main test
-
+// Main
 async function main() {
   let succces = 0;
   let runs = argv['runs'] || 3;
+  let report = argv['report'];
   let results = [];
+
+  if (!runs) {
+    printUsage();
+    return;
+  }
 
   for (let i = 1; i <= runs; i++) {
     try {
       loggerStep = 1;
       console.log(`------ Run ${i} ------`);
-      await runGoogleSearchTest(ampTestConfig, {
+      await runTask(taskConfig, {
         run: i,
-        screenshot: true,
       });
       succces++;
       results.push('success');
