fe5264bdb796e992eb02f32409e62f8ef8e5af90
Jonathan Chen <jonoson@gmail.com>
2019-01-26 22:22:34 -0500

    update main.js

--- main.js
+++ main.js
@@ -5,77 +5,137 @@ const {
   By,
   Key,
   until,
+  Origin,
   logging
 } = require('selenium-webdriver');
 const chrome = require('selenium-webdriver/chrome');
+const firefox = require('selenium-webdriver/firefox');
 const assert = require('assert');
+const argv = require('minimist')(process.argv.slice(2));
 
 logging.installConsoleHandler();
 logging.getLogger('webdriver.http').setLevel(logging.Level.SEVERE);
 
 let ampTestConfig = {
-  searchKeyword: 'amp by example',
+  browser: 'chrome',
+  searchKeyword: 'amp-list',
   expectedAmpViewerTitle: 'ampbyexample.com',
+  targetSelector: 'a.amp_r',
 };
 
 const width = 500;
 const height = 1000;
+let loggerStep = 1;
 
-async function runGoogleSearchTest(config) {
+function log(message) {
+  console.log(`${loggerStep}: ${message}`);
+  loggerStep++;
+}
+
+async function createDriver(browser) {
   let driver;
   try {
-    driver = await new Builder()
-      .forBrowser('chrome')
-      .setChromeOptions(
-        new chrome.Options().setMobileEmulation({
-          deviceName: 'Nexus 5X'
-        }).windowSize({
-          width,
-          height
-        }))
-      .build();
+    switch(browser) {
+      case 'firefox':
+        let options = new firefox.Options()
+            // .setPreference('devtools.responsive.html.displayedDeviceList',
+            //   '{"added":["Nexus 5X"],"removed":[]}')
+            .setPreference('devtools.responsive.userAgent',
+              'Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1')
+            .setBinary(firefox.Channel.RELEASE);
+        driver = new Builder()
+          .forBrowser('firefox')
+          .setFirefoxOptions(options)
+          .build();
+        break;
 
-    console.log('Search...');
+      case 'chrome':
+      default:
+        driver = new Builder()
+        .forBrowser('chrome').setChromeOptions(
+          new chrome.Options()
+          .setMobileEmulation({
+            deviceName: 'Pixel 2'
+          }).windowSize({
+            width,
+            height
+          }))
+        .build();
+        break;
+    }
+  } catch(err) {
+    console.error(err);
+  } finally {
+    return driver;
+  }
+}
 
-    await driver.get('http://www.google.com');
+async function runGoogleSearchTest(config, context) {
+  let driver;
+  try {
+    driver = await createDriver(config.browser);
+
+    await driver.manage().window().setRect({
+      width: width,
+      height: height,
+      x: 100,
+      y: 0
+    });
+
+    const actions = driver.actions({
+      bridge: true
+    });
+
+    log('Search...');
+    await driver.sleep(2000);
+
+    await driver.get('https://www.google.com/ncr');
     await driver.findElement(By.name('q')).sendKeys(config.searchKeyword, Key.RETURN);
+
     await driver.wait(until.titleIs(config.searchKeyword + ' - Google Search'), 3000);
-    console.log('Received search results.');
 
-    // console.log('sleep 2000');
-    // await driver.sleep(2000);
+    log('Received search results.');
 
-    console.log('Click on search input.');
-    let searchInput = await driver.findElement(By.name('q'));
-    await searchInput.click();
+    let body = await driver.wait(until.elementLocated(By.css('body')), 3000);
+    let mainDom = await driver.wait(until.elementLocated(By.css('#tophf')), 3000);
 
-    // console.log('sleep 2000');
-    // await driver.sleep(2000);
+    log('mainDom result found.');
 
-    let firstAmpResult = await driver.wait(until.elementLocated(By.css('#gsr a.amp_r')), 10000);
-    console.log('Found first amp result.');
+    let targetDom = await driver.findElement(By.css(config.targetSelector));
+    log('first amp result found.');
 
-    console.log('tag:', await firstAmpResult.getTagName());
-    console.log('class:', await firstAmpResult.getAttribute('class'));
-    console.log('href:', await firstAmpResult.getAttribute('href'));
-    console.log('text:', await firstAmpResult.getText());
+    assert.equal(await targetDom.getTagName(), 'a');
+    assert.equal(await targetDom.getAttribute('class'), 'C8nzq BmP5tf amp_r');
 
-    // console.log('sleep 2000');
-    // await driver.sleep(2000);
+    await targetDom.click();
+    log('Clicked first AMP result.');
 
-    console.log('Click first AMP result.');
-    await firstAmpResult.click();
+    await driver.sleep(2000);
 
-    let ampViewerTitle = await driver.findElement(By.css('.amp-ttltxt'));
-    await driver.wait(until.elementIsVisible(ampViewerTitle), 3000);
-
-    console.log(await ampViewerTitle.getText());
+    let ampViewerTitle = await driver.wait(until.elementLocated(By.css('.amp-ttltxt')), 3000);
     assert.equal(await ampViewerTitle.getText(), config.expectedAmpViewerTitle);
+    log('Saw AMP viewer title: ' + await ampViewerTitle.getText());
 
-    await driver.sleep(1000);
+    // Switch to first iframe.
+    // driver.switchTo().frame(0);
+    //
+    // let ampContent = await driver.wait(until.elementLocated(By.css('img[alt="AMP by Example"]')), 3000);
+    // log('Saw AMP by example content.');
+    await driver.sleep(3000);
+
+    log('Task Complete.');
+
+  } catch (err) {
+    console.error(err);
 
   } finally {
+    if (driver && context.screenshot) {
+      let image = await driver.takeScreenshot();
+      require('fs').writeFileSync(`output/run-${context.run}.png`, image, 'base64');
+      log('Screenshot took.');
+    }
     await driver && driver.quit();
+    log('Driver quit.');
   }
 }
 
@@ -83,18 +143,30 @@ async function runGoogleSearchTest(config) {
 
 async function main() {
   let succces = 0;
-  let numTests = 10;
-  for (let i = 0; i < numTests; i++) {
+  let runs = argv['runs'] || 3;
+  let results = [];
+
+  for (let i = 1; i <= runs; i++) {
     try {
-      console.log(`------ Round ${i} ------`);
-      await runGoogleSearchTest(ampTestConfig);
+      loggerStep = 1;
+      console.log(`------ Run ${i} ------`);
+      await runGoogleSearchTest(ampTestConfig, {
+        run: i,
+        screenshot: true,
+      });
       succces++;
+      results.push('success');
     } catch (err) {
       console.error(err);
+      results.push('failed');
     }
   }
   console.log('===========');
-  console.log(`succces: ${succces}/${numTests}`);
+  console.log(`succces: ${succces}/${runs}`);
+
+  for (let i = 0; i < runs; i++) {
+    console.log(`${i+1}: ${results[i]}`);
+  }
 }
 
 main();
